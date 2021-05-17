8b12b364ea5f7df0dbe5d1b556f39fe39965c092
Alex-Cauble <alexander.t.cauble@gmail.com>
2021-05-17 08:18:48 -0500

    fix: some problems with formatting

--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -25,6 +25,6 @@ export class Task extends BaseEntity {
   @ManyToOne(() => User, (user) => user.tasks, { eager: false })
   user: User;
 
-  @Column()
+  @Column({ name: 'user_id' })
   userId: number;
 }
